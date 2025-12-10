3a1f6f262ead33ad12fecce51cca68943c159b54
FX-137 <fernandodecastroleonhardt@gmail.com>
2025-12-09 21:25:08 -0300

    Update data.js
    Mudar Big agapio

--- data.js
+++ data.js
@@ -475,7 +475,7 @@ export const MENU_DATA: MenuCategory[] = [
         name: "Big Agápio",
         description: "Pão massinha, 2 hambúrgueres bovinos, tomate, alface, milho, ervilha, palmito, pepino, presunto, queijo, ovo e maionese.",
         price: 30.00,
-        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgTkS-tpqWFbQJ90d0AhYD8aFPNvaEr17HQVwyFwDvhRbTutHAoYLLJ68IU80sQdOB-njylmFm-C5-dS8QkktQhwkpLXnpBdF18C_kMVDDm_5zakHs_uQHfx62ilw3-mY2pZZp3Ub-QLD-njQEix0sSBK40KupsY4j9_ABrouQVI53CxLQM0g41BXhMwXk/s1536/Big%20agapio%20chat.png"
+        image: " https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgtUQpM4xh5RICao7U9v220rLNNyGSJxONPd9FraLUtxhZ1GBK1VUN_U4Jr6j6jYA4tQr1ES4f4ONWNqTo1WypAdIBMgjglBTPn5725qFoK2QzmbrHlpaKrQHK9EDwoISSAlkln2YRn41RQC-q6ZKKPipHPzFfzdLsTJRFY6cQh6wC-Ssoy4xya3cwJmr8/s1536/Novo%20Big%20agapio%203.png"
       },
       {
         id: "MP3",
